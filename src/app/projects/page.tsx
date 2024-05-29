4a3d2af67cc05bd1e4f74d452d102a73559c2750
Eduardo <eddieegomez4@gmail.com>
2024-05-29 08:53:57 -0700

    update projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -11,6 +11,7 @@ import hdzero from '@/images/screenshots/hdzero.webp'
 import rickandmorty from '@/images/screenshots/rickandmorty.png'
 import reactopenai from '@/images/screenshots/reactopenai.png'
 import matchinggame from '@/images/screenshots/matchinggame.png'
+import tictactoe from "@/images/screenshots/tic-tac-toe.png"
 
 const projects = [
   {
@@ -26,6 +27,13 @@ const projects = [
       'Application tool to lend or borrow gear from friends with automated reminders - WIP',
     link: { href: 'https://github.com/iameddieyayaya/who-has-my-shit', label: 'github.com' },
     logo: whohasmyshit,
+  },
+  {
+    name: `Tic Tac Toe Python`,
+    description:
+      'Simple Tic Tac Toe Game in Python using pygame',
+    link: { href: 'https://github.com/iameddieyayaya/who-has-my-shit', label: 'github.com' },
+    logo: tictactoe,
   },
   {
     name: `React OpenAI Vision`,
@@ -58,11 +66,6 @@ const projects = [
       label: "github.com"
     },
     logo: hdzero,
-  }, {
-    name: 'Trinity',
-    description: `An application that trades/mines flo and raven crypto currencies. Using the Mining Rig Rental, Nicehash, and Bittrex APIs.`,
-    link: { href: 'https://github.com/iameddieyayaya/trinity', label: 'github.com' },
-    logo: 'https://i.ibb.co/Th8Bbyb/screenshot1.png',
   },
   {
     name: "Morty-Dex",
@@ -81,6 +84,12 @@ const projects = [
       label: "github.com"
     },
     logo: matchinggame,
+  },
+  {
+    name: 'Trinity',
+    description: `An application that trades/mines flo and raven crypto currencies. Using the Mining Rig Rental, Nicehash, and Bittrex APIs.`,
+    link: { href: 'https://github.com/iameddieyayaya/trinity', label: 'github.com' },
+    logo: 'https://i.ibb.co/Th8Bbyb/screenshot1.png',
   },
   {
     name: 'Crwn Apparel',
